395921ee562a6e9375b435d46948294f0886b1d9
PokemonHeartGoldEnjoyer <e.simakou@donboscosandona.it>
2025-06-18 10:12:37 +0200

    End of Navbar and Hero, Started with Cards

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+    def index
+    end
+    def Spain
+    end
+    def France
+    end
+    def Germany
+    end
+    def Albania
+    end
+    def Greece
+    end
+end
